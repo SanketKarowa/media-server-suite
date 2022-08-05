4d8bfec5f66f33a7656caa70f1b32014e2116cd1
Sachin Oraon <sachinoraon55@gmail.com>
2022-08-05 13:49:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 set -e
-echo $(date "+%F %T")': Starting docker start script' >> /home/pi/docker/log.txt
+
+LOG_FILE=/home/pi/docker/log.txt
+DEV_PATH=/dev/sda1
+CONTAINERS=( filebrowser qbittorrent h5ai aria2-pro qbot )
+
+echo $(date "+%F %T")': Starting docker start script' >> $LOG_FILE
 for i in {1..5}
 do
-  if [ `lsblk /dev/sda1 --output MOUNTPOINT | wc -l` -eq 2 ]
+  if [ `lsblk ${DEV_PATH} --output MOUNTPOINT | wc -l` -eq 2 ]
   then
-    echo `date "+%F %T"`': /dev/sda1 mount point found, starting containers' >> /home/pi/docker/log.txt
+    echo `date "+%F %T"`': mount point for '${DEV_PATH}' found, starting containers' >> $LOG_FILE
     if [ `systemctl is-active docker.service` == 'active' ]
     then
-       docker start filebrowser qbittorrent h5ai aria2-pro qbot
-       if [ `docker ps --filter "status=running" --filter "name=h5ai" --filter "name=filebrowser" --filter "name=qbittorrent" --filter "name=aria2-pro" --filter "name=qbot" -q | wc -l` -eq 5 ]
-       then
-         echo `date "+%F %T"`': Containers started successfully' >> /home/pi/docker/log.txt
-         break
-       fi
+       for container in "${CONTAINERS[@]}"
+       do
+           docker start ${container}
+           if [ `docker ps --filter "status=running" --filter "name=${container}" -q | wc -l` -eq 1 ]; then echo `date "+%F %T"`': '${container}' started successfully' >> $LOG_FILE; fi
+       done
+       break
     else
-      echo `date "+%F %T"`': docker service not active, Retrying' >> /home/pi/docker/log.txt
+      echo `date "+%F %T"`': docker service not active, Retrying' >> $LOG_FILE
     fi
   else
-    echo `date "+%F %T"`': /dev/sda1 not found, Retrying' >> /home/pi/docker/log.txt
+    echo `date "+%F %T"`': mount point for '${DEV_PATH}' NOT found, Retrying' >> $LOG_FILE
   fi
   sleep 10
 done
-echo `date "+%F %T"`': Completed docker start script' >> /home/pi/docker/log.txt
+echo `date "+%F %T"`': Completed docker start script' >> $LOG_FILE
